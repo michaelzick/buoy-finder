721d12f8023ac223475421bcf6d50d81427725a5
Michael Zick <michaelzick@gmail.com>
2018-01-27 18:47:42 +0000

    Revert "FAVS: got infoWin rendering as jsx; onclick firing too soon"
    This reverts commit 3418198df4b1296ba735458a7e86782da30531f9.

--- src/containers/Map/Map.js
+++ src/containers/Map/Map.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import ReactDOMServer from 'react-dom/server';
 import loadGoogleMapsAPI from 'load-google-maps-api';
 import Sidebar from '../../components/Sidebar/Sidebar';
-import InfoWindow from '../../components/InfoWindow/InfoWindow';
 import classes from './Map.css';
 
 class Map extends Component {
@@ -39,14 +37,14 @@ class Map extends Component {
     return georssLayer;
   }
 
-  addToFavs = (markerData) => {
+  addToFavs = (newData) => {
     const favsJson = {};
 
-    // Add the markerData object to an id key for storage
-    favsJson[markerData.id] = markerData;
+    // Add the newData object to an id key for storage
+    favsJson[newData.id] = newData;
 
     // Set the item in storage (has to be string)
-    localStorage.setItem(markerData.id, JSON.stringify(favsJson));
+    localStorage.setItem(newData.id, JSON.stringify(favsJson));
 
     this.setState({
       favs: localStorage
@@ -67,16 +65,43 @@ class Map extends Component {
   // Action: marker click
   addDomControls = georssLayer => {
     georssLayer.addListener('click', (e) => {
-      // Pass in a copy of the data, not the reference
-      const newData = {...e.featureData},
-            infoWindowHtml = <InfoWindow
-                               element={e}
-                               data={newData}
-                               addFav={this.addToFavs}
-                             />;
+      const newData = {...e.featureData}, // Copy the feature data first
+          infoWinDiv = document.createElement('div'),
+          clearDiv = document.createElement('div'),
+          favBarDiv = document.createElement('div'),
+          favBarPlusDiv = document.createElement('div'),
+          favBarTextDiv = document.createElement('div'),
+          favBarText = document.createTextNode('Add to favorites'),
+          favBarPlus = document.createTextNode('+');
+
+      // Div to clear floats (utility);
+      clearDiv.classList.add(classes.clearDiv);
+
+      favBarTextDiv.appendChild(favBarText); // Add the text to the span element
+      favBarPlusDiv.appendChild(favBarPlus); // Add the '+' to the div element
+
+      // Adds classes to the elements
+      favBarTextDiv.classList.add(classes.favBarText);
+      favBarPlusDiv.classList.add(classes.favBarPlus);
+
+      // Clicking '+' updates state and adds to favs
+      favBarPlusDiv.addEventListener('click', () => {
+        this.addToFavs(newData);
+      });
+
+      favBarDiv.appendChild(favBarTextDiv); // Add the text to the parent div
+      favBarDiv.appendChild(favBarPlusDiv); // Add the '+' to the parent div
+      favBarDiv.appendChild(clearDiv); // Add the clearDiv to the parent div
+
+      infoWinDiv.appendChild(favBarDiv);
+
+      infoWinDiv.insertAdjacentHTML('beforeend', e.featureData.infoWindowHtml);
+
+      // Add custom html to the infoWindow
+      newData.infoWindowHtml = infoWinDiv;
 
       // Set the infoWindow html
-      e.featureData.infoWindowHtml = ReactDOMServer.renderToString(infoWindowHtml);
+      e.featureData.infoWindowHtml = newData.infoWindowHtml;
     });
   }
 
