86657b5428511337e5185b9a7c94d7da7792856f
Michael Zick <michaelzick@gmail.com>
2018-01-27 18:47:27 +0000

    Revert "INFOWIN: removed big comment block"
    This reverts commit e3bd12924c3aab2565ca24ceb3aa430354b2b2de.

--- src/components/InfoWindow/InfoWindow.js
+++ src/components/InfoWindow/InfoWindow.js
@@ -13,3 +13,41 @@ const infoWindow = (props) => {
 };
 
 export default infoWindow;
+
+
+
+
+// const newData = {...e.featureData}, // Copy the feature data first
+//           infoWinDiv = document.createElement('div'),
+//           clearDiv = document.createElement('div'),
+//           favBarDiv = document.createElement('div'),
+//           favBarPlusDiv = document.createElement('div'),
+//           favBarTextDiv = document.createElement('div'),
+//           favBarText = document.createTextNode('Add to favorites'),
+//           favBarPlus = document.createTextNode('+');
+
+//       // Div to clear floats (utility);
+//       clearDiv.classList.add(classes.clearDiv);
+
+//       favBarTextDiv.appendChild(favBarText); // Add the text to the span element
+//       favBarPlusDiv.appendChild(favBarPlus); // Add the '+' to the div element
+
+//       // Adds classes to the elements
+//       favBarTextDiv.classList.add(classes.favBarText);
+//       favBarPlusDiv.classList.add(classes.favBarPlus);
+
+//       // Clicking '+' updates state and adds to favs
+//       favBarPlusDiv.addEventListener('click', () => {
+//         this.addToFavs(newData);
+//       });
+
+//       favBarDiv.appendChild(favBarTextDiv); // Add the text to the parent div
+//       favBarDiv.appendChild(favBarPlusDiv); // Add the '+' to the parent div
+//       favBarDiv.appendChild(clearDiv); // Add the clearDiv to the parent div
+
+//       infoWinDiv.appendChild(favBarDiv);
+
+//       infoWinDiv.insertAdjacentHTML('beforeend', e.featureData.infoWindowHtml);
+
+//       // Add custom html to the infoWindow
+//       newData.infoWindowHtml = infoWinDiv;
