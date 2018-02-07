2b38a13bf08bbe1d507dc121204e05d18998d8ec
Michael Zick <michaelzick@gmail.com>
2018-02-07 21:30:21 +0000

    TEST: deleteFav unit test

--- src/containers/Map/Map.test.js
+++ src/containers/Map/Map.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Map } from './Map';
-// import loadGoogleMapsAPI from 'load-google-maps-api';
 
 configure({adapter: new Adapter()});
 
@@ -21,6 +20,9 @@ const localStorageMock = (() => {
     },
     removeItem: (key) => {
       delete store[key];
+    },
+    getStore: () => {
+      return store;
     }
   };
 })();
@@ -29,7 +31,7 @@ const localStorageMock = (() => {
 Object.defineProperty(window, 'localStorage', {value: localStorageMock});
 
 describe ('Map.addToFavs', () => {
-  it ('should add fav to localStorage and state', () => {
+  it('should add fav to localStorage and state', () => {
     const wrapper = shallow(<Map localstorage={window.localStorage} />),
           instance = wrapper.instance(),
           element = document.createElement('div'),
@@ -57,24 +59,45 @@ describe ('Map.addToFavs', () => {
   });
 });
 
-// describe ('Map.initMap', () => {
-//   it ('should return a geoRSS layer', () => {
-//     const wrapper = shallow(<Map localstorage={{}} />),
-//           instance = wrapper.instance(),
-//           spy = jest.spyOn(instance, 'getGeoRssLayer');
+describe ('Map.deleteFav', () => {
+  it('should remove fav from localStorage and state', () => {
+    const wrapper = shallow(<Map localstorage={window.localStorage} />),
+          instance = wrapper.instance(),
+          element = document.createElement('div'),
+          dataA = {
+            description: 'the description',
+            id: '123',
+            infoWindowHtml: '<div id="info-window-html"></div>',
+            name: 'Buoy A',
+            snippet: 'Feb 5, 2018 snippet',
+            status: 'OK'
+          },
+          dataB = {
+            description: 'another description',
+            id: '456',
+            infoWindowHtml: '<div id="info-window-html"></div>',
+            name: 'Buoy B',
+            snippet: 'Feb 7, 2018 snippet',
+            status: 'OK'
+          };
 
-//     window.localStorage.setItem('foo', 'bar');
-//     console.log(window.localStorage);
+    let storeKeysLength = 0;
 
-//     return loadGoogleMapsAPI().then(googleMaps => {
+    element.target = {
+      innerHTML: '+'
+    };
 
-//       // console.log(spy);
+    // Add favs
+    instance.addToFavs(element, dataA);
+    instance.addToFavs(element, dataB);
 
-//       // expect.assertions(1);
-//       expect(instance.getGeoRssLayer).toHaveBeenCalled();
-//       // expect (typeof (instance.getGeoRssLayer(googleMaps)).toBe('string'))
-//     }).catch(err => {
-//       // console.error(err);
-//     });
-//   });
-// });
+    // Remove a fav
+    instance.deleteFav(dataA.id);
+
+    // Get the number of keys in the localStorage store object
+    storeKeysLength = Object.keys(window.localStorage.getStore()).length;
+
+    // Checks that state.favs is equal to window.localStorage
+    expect(storeKeysLength).toEqual(1);
+  });
+});
